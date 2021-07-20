f28c5bfe7ed205027edc60976c370275202e3cac
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-20 12:59:12 -0300

    Class 255 - Using useEffect with Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -8,6 +8,14 @@ import { IStore } from './store';
 
 const App = () => {
   const { cartVisible: showCart } = useSelector((state: IStore) => state.ui);
+  const { cart } = useSelector((state: IStore) => state);
+
+  useEffect(() => {
+    fetch('https://react-tcg-14-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
