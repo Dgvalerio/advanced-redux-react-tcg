8eadcbb5cfcd42521f9bb06aa2294b46eef3b4d4
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-20 13:43:06 -0300

    Class 258 - Using an Action Creator Thunk

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
 import { IStore } from './store';
-import { uiActions } from './store/ui';
+import { sendCartData } from './store/cart';
 
 let isInitial = true;
 
@@ -21,44 +21,8 @@ const App = () => {
       isInitial = false;
       return;
     }
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending',
-        message: 'Sending cart data!',
-      })
-    );
-    const sendCartData = async () => {
-      const response = await fetch(
-        'https://react-tcg-14-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
 
-      if (!response.ok) throw new Error('Sending cart data failed!');
-    };
-
-    sendCartData()
-      .then(() =>
-        dispatch(
-          uiActions.showNotification({
-            status: 'success',
-            title: 'Success!',
-            message: 'Sending cart data successfully!',
-          })
-        )
-      )
-      .catch(() =>
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error!',
-            message: 'Sending cart data failed!',
-          })
-        )
-      );
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { ICartItem, IItem } from '../interfaces';
+import { uiActions } from './ui';
 
 const initialState = { items: [] };
 
@@ -45,6 +46,47 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart: ICartState) => async (dispatch: any) => {
+  dispatch(
+    uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending',
+      message: 'Sending cart data!',
+    })
+  );
+
+  const sendRequest = async () => {
+    const response = await fetch(
+      'https://react-tcg-14-default-rtdb.firebaseio.com/cart.json',
+      {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+
+    if (!response.ok) throw new Error('Sending cart data failed!');
+  };
+
+  try {
+    await sendRequest();
+    dispatch(
+      uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sending cart data successfully!',
+      })
+    );
+  } catch {
+    dispatch(
+      uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      })
+    );
+  }
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
