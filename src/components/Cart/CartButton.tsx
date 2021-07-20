2e454d213b16ee3d82478dbe9d962c4a06b71435
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-20 12:03:39 -0300

    Update quantity in header component

--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { IStore } from '../../store';
 import { cartActions } from '../../store/cart';
 import classes from './CartButton.module.css';
 
@@ -9,10 +10,20 @@ const CartButton = () => {
 
   const handleToggleCart = () => dispatch(cartActions.toggleCart());
 
+  // Se for por tipo de item
+  // const { length: quantity } = useSelector((state: IStore) => state.cart.items);
+
+  // Se for items independentemente do tipo
+  const { items } = useSelector((state: IStore) => state.cart);
+  const quantity = items.reduce(
+    (accumulator, item) => accumulator + item.quantity,
+    0
+  );
+
   return (
     <button type="button" className={classes.button} onClick={handleToggleCart}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
   );
 };
